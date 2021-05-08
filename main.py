c127d4561a8794ceb6737fc2dd6a833d11d9af68
Atif <contact@atifanwer.xyz>
2021-05-08 13:52:59 +0200

    Dataset Loading incl white balance working

--- main.py
+++ main.py
@@ -19,12 +19,6 @@ CUDA 11.3
 cuDnn 8.0
 """
 from typing import List
-
-from numpy import hstack
-from numpy import zeros
-from numpy import ones
-from numpy.random import rand
-from numpy.random import randn
 from matplotlib import pyplot
 from keras.utils.vis_utils import plot_model
 import matplotlib as plt
@@ -37,7 +31,7 @@ import math
 import sys
 import os
 import glob
-
+import imutils
 import cv2
 import keras.backend as K
 import tensorflow as tf
@@ -53,8 +47,12 @@ from keras.backend import mean
 from keras.models import Model, model_from_json
 from keras.utils import plot_model
 from keras.engine.topology import Network
-
 from collections import OrderedDict
+# Removes error when running Tensorflow on GPU
+# for Tensorflow 2.2 and Python 3.6+
+from tensorflow._api.v2.compat.v1 import ConfigProto
+from tensorflow._api.v2.compat.v1 import InteractiveSession
+
 
 # ----------------------------------------
 # SETUP GPU
@@ -63,11 +61,6 @@ from collections import OrderedDict
 print(tf.test.is_built_with_cuda())
 print("Num GPUs Available: ", len(tf.config.list_physical_devices('GPU')))
 tf.config.list_physical_devices('GPU')
-
-# Removes error when running Tensorflow on GPU
-# for Tensorflow 2.2 and Python 3.6+
-from tensorflow._api.v2.compat.v1 import ConfigProto
-from tensorflow._api.v2.compat.v1 import InteractiveSession
 
 config = ConfigProto()
 config.gpu_options.allow_growth = True
@@ -94,8 +87,7 @@ def load_dataset():
     # Loading the data. There will be two types:
     # 1. Polarized PNG files (0, 45, 90, 135 degree)
     # 2. Generate estimated diffuse image
-    # After loading the dataset, we can resize the images to smaller resolution.
-    # Starting small resolution for a faster training and then extend to bigger size later
+    # 3. The images are already resized in the read_image function
 
     # filepath = os.path.realpath(__file__)
     filepath = "/home/atif/Documents/Datasets/PolarizedImages/"
@@ -107,6 +99,7 @@ def load_dataset():
 
     polarization_labels = ['0', '45', '90', '135']
 
+    OriginalImageStack, height, width, channels = read_images(sourceFolder, pattern="*_Itot.png")
     imgStack_0deg, height, width, channels = read_images(sourceFolder, pattern="*_0.png")
     imgStack_45deg, height, width, channels = read_images(sourceFolder, pattern="*_45.png")
     imgStack_90deg, height, width, channels = read_images(sourceFolder, pattern="*_90.png")
@@ -124,7 +117,8 @@ def load_dataset():
     r = []
     estimated_diffuse_stack = []
     i = 0
-    for img0, img45, img90, img135 in zip(imgStack_0deg, imgStack_45deg, imgStack_90deg, imgStack_135deg):
+    for orig, img0, img45, img90, img135 in zip(OriginalImageStack, imgStack_0deg, imgStack_45deg, imgStack_90deg,
+                                                imgStack_135deg):
         # Note: Each img variable is a 3 channel image; so we can split it up in BGR
         blue, green, red = cv2.split(img0)
         b.append(blue)
@@ -150,20 +144,6 @@ def load_dataset():
         g_min = np.amin(g, axis=0)
         r_min = np.amin(r, axis=0)
 
-        # DEBUG STACK DISPLAY
-        # Horizontal1 = np.hstack([b[:, :, i],  g[:, :, i], r[:, :, i]])
-        # Debug display
-        cv2.namedWindow("Loaded Image", cv2.WINDOW_NORMAL)
-        # cv2.resizeWindow("dice", 600,600)
-        cv2.imshow("Loaded Image", b_min)
-        cv2.waitKey(0)
-        cv2.imshow("Loaded Image", g_min)
-        cv2.waitKey(0)
-        cv2.imshow("Loaded Image", r_min)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
-        # i += 1
-
         merged = cv2.merge([b_min, g_min, r_min])
         i += 1
 
@@ -173,29 +153,61 @@ def load_dataset():
         # Stack the estimated diffuse images for later use in loop
         estimated_diffuse_stack.append(merged)
 
+        # DEBUG STACK DISPLAY
+        Horizontal1 = np.hstack([orig, merged])
+        # Debug display
+        cv2.namedWindow("Loaded Image", cv2.WINDOW_NORMAL)
+        # cv2.resizeWindow("dice", 600,600)
+        cv2.imshow("Loaded Image", Horizontal1)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        # i += 1
+
         # clear data before next loop; avoiding any data overwriting issues
         b.clear()
         g.clear()
         r.clear()
         merged.fill(0)  # clear the vars before calculating
-    return img_0deg, img_45deg, img_90deg, img_135deg, estimated_diffuse_stack
+    # Returns all the 4xpolarized images and their estimated diffuse images (Total 5 images)
+    return OriginalImageStack, imgStack_0deg, imgStack_45deg, imgStack_90deg, imgStack_135deg, estimated_diffuse_stack
 
 
 # ----------------------------------------
+# Read all images in the dataset and return a np array
 def read_images(path, pattern):
     image_stack = []
     # build path string, sort by name
     for img in sorted(glob.glob(path + "/" + pattern)):
-        image_stack.append(cv2.imread(img))
+        img = cv2.imread(img)
+        # Resize image to improve performance
+        resized_image = resize_images(img, rowsize=256, colsize=256)
+        image_stack.append(resized_image)
 
     height, width, channels = image_stack[0].shape
     return image_stack, height, width, channels
 
 
 # ----------------------------------------
-def resize_images(rowsize, colsize):
-    # The loaded images will
-    return
+def resize_images(img, rowsize, colsize):
+    # The loaded images will be resized to lower res for faster training and eval. After POC, higher res can be used
+    # rows, cols, ch = img.shape
+    # Adding white balance to remove the green tint generating from the polarized images
+    resized_image = white_balance(imutils.resize(img, width=colsize, height=rowsize))
+    return resized_image
+
+
+# ------------------------------------------
+# White balance
+#  Source: https://stackoverflow.com/questions/46390779/automatic-white-balancing-with-grayworld-assumption
+# ------------------------------------------
+def white_balance(input_image):
+    result = cv2.cvtColor(input_image, cv2.COLOR_RGB2LAB)
+    avg_a = np.average(result[:, :, 1])
+    avg_b = np.average(result[:, :, 2])
+    result[:, :, 1] = result[:, :, 1] - ((avg_a - 128) * (result[:, :, 0] / 255.0) * 1.1)
+    result[:, :, 2] = result[:, :, 2] - ((avg_b - 128) * (result[:, :, 0] / 255.0) * 1.1)
+    whiteBalancedImage = cv2.cvtColor(result, cv2.COLOR_LAB2RGB)
+    return whiteBalancedImage
 
 
 # ----------------------------------------
@@ -236,7 +248,8 @@ def train(generator, discriminator, gan_model, latent_dim):
 # To setup the various variables for the network
 setup_options()
 # Load the dataset with resized polar and estimated diffuse images
-img_0deg, img_45deg, img_90deg, img_135deg, estimated_diffuse = load_dataset()
+OriginalImageStack, img_0deg, img_45deg, img_90deg, img_135deg, estimated_diffuse = load_dataset()
+print("[LOADED IMAGE] - Dataset size: ", len(img_0deg), "images")
 latent_dim = 5
 # ----------------------------------------
 # create discriminator
